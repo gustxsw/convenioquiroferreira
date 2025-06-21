609461669b2f9128172d95fb173ee5a23e65f69c
gustxsw <161257333+gustxsw@users.noreply.github.com>
2025-06-21 12:20:29 -0300

    Added Design sem nome.png

---
+++ dist/sw.js
@@ -0,0 +1,49 @@
+// Service Worker para PWA
+const CACHE_NAME = 'quiro-ferreira-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/favicon.ico',
+  '/logo_quiroferreira.svg'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
