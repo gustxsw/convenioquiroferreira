c3b40f6ffb29ece548b59c0f704f03001e4b5c97
gustxsw <gustavocandido044@gmail.com>
2025-08-12 21:27:00 -0300

    Update database connection string in db.js

--- server/db.js
+++ server/db.js
@@ -6,5 +6,5 @@ dotenv.config();
 export const pool = new pg.Pool({
   connectionString:
     process.env.DATABASE_URL ||
-    "postgresql://neondb_owner:npg_FC9TuaYLdMD8@ep-steep-violet-afyt4sti-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "postgresql://neondb_owner:npg_hZTr3D2oiFAv@ep-bold-grass-acq6z6br-pooler.sa-east-1.aws.neon.tech/convenioquiroferreira?sslmode=require&channel_binding=require",
 });
