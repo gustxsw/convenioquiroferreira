bc6e99b882043e85a8f5dda22982b38ea5258cbd
gustxsw <161257333+gustxsw@users.noreply.github.com>
2025-08-20 09:52:30 -0300

    Corrigir erro de path não definido

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
