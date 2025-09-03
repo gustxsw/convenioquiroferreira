301f66982aa6a63d10e07a191ce02f23d47b0c0b
gustxsw <161257333+gustxsw@users.noreply.github.com>
2025-09-03 11:54:22 -0300

    Fix syntax error in DependentsSection.tsx

--- src/pages/client/DependentsSection.tsx
+++ src/pages/client/DependentsSection.tsx
@@ -159,7 +159,6 @@ const DependentsSection: React.FC<DependentsSectionProps> = ({ clientId }) => {
           headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
-          },
       .filter((d) => d.subscription_status === "active")
             client_id: clientId,
             name,
